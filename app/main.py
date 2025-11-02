5cb7b2d3a4cf322cc3872854b4a09518145ec5fb
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-11-02 15:41:09 +0100

    feat: implement get_weather function sending request to weather api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+import requests
+
+API_KEY = os.environ.get("API_KEY")
+
+BASE_URL = "http://api.weatherapi.com/v1"
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        print("API_KEY not found in environment variables.")
+        return
+
+    city_input = input("Enter city name or press Q to exit: ").strip()
+
+    if city_input.lower() == "q":
+        print("Exiting...")
+    else:
+        url = f"{BASE_URL}/current.json?key={API_KEY}&q={city_input}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        name = data["location"]["name"]
+        country = data["location"]["country"]
+        temp_c = data["current"]["temp_c"]
+        temp_f = data["current"]["temp_f"]
+
+        print(f"Weather in {name}, {country}: {temp_c}°C ({temp_f}°F)")
+
+        get_weather()
 
 
 if __name__ == "__main__":
